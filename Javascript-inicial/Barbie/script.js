56474f8b6fcd52e6cf54e64c6fb59824d39964e5
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-27 16:52:41 -0300

    revisão

---
+++ Javascript-inicial/Barbie/script.js
@@ -0,0 +1,36 @@
+function enviar(){
+    document.getElementById("nome").style.border = "1px solid black"
+document.getElementById("assunto").style.border = "1px solid black"
+const nome = document.getElementById("nome").value
+const assunto = document.getElementById("assunto").value
+
+if(!nome || !assunto){
+    document.getElementById("nome").style.border = "2px solid red"
+    document.getElementById("assunto").style.border = "2px solid red"
+    return
+}
+
+const mensagem = `Gostaria de entrar em contato! \n\nNome: ${nome} \nAssunto: ${assunto}`
+const msg = encodeURIComponent(mensagem)
+window.location.href = `https://wa.me/5541998245828?text=${msg}`
+}
+
+
+function toggleMute() {
+    var music = document.getElementById("background-music");
+    var button = document.getElementById("mute-button");
+    
+    if (music.muted) {
+        music.muted = false;
+        button.textContent = "🔊"; 
+    } else {
+        music.muted = true;
+        button.textContent = "🔇"; 
+    }
+}
+
+
+window.onload = function() {
+    var music = document.getElementById("background-music");
+    music.play();
+}
